767cdf070cc1e2347892981bccb635cb9e4add6a
Fomin Mikhail <m.fomin@elama.ru>
2023-04-18 15:43:30 +0400

    data

---
+++ knowledge/code/jaba.md
@@ -0,0 +1,2 @@
+# Java 8 vs Java 17
+https://habr.com/ru/articles/591159/
---
+++ knowledge/code/javatest/src/main/java/home/test/javapure/PureJavaTest.java
@@ -0,0 +1,10 @@
+package home.test.javapure;
+
+class PureJavaTest {
+
+    public static void main(String[] args) {
+        System.out.println("Soooqa, I miss kotlin already!");
+    }
+
+}
+
